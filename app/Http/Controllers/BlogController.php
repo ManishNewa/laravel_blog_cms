f357d68b0c1a5f6e25f4e6686d47a76009a717e2
Manish Maharjan <manishnewar26@gmail.com>
2019-04-22 12:56:24 +0545

    Left at the post ordering

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -10,7 +10,7 @@ class BlogController extends Controller
 {
     public function index(){
         
-        $posts = Post::all();
+        $posts = Post::with('author')->latestFirst()->get();
         return  view('blog.index',compact('posts'));
 
     }
